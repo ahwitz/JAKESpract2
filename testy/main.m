5ec4994dde37603f7cd7b863b28d093da61a07fa
ahwitz <ahwitz@gmail.com>
2013-02-08 15:21:45 -0500

    this is my first commit

---
+++ testy/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  testy
+//
+//  Created by Andrew Horwitz on 2/8/13.
+//  Copyright (c) 2013 Andrew Horwitz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World! Makin a change in the file.");
+        
+    }
+    return 0;
+}
+
